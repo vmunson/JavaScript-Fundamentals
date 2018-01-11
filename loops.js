e4903c5ff891cd61d726a9d3672115b85fcfb021
vmunson <virginia.munson@gmail.com>
2018-01-11 11:49:46 -0500

    loop examples

---
+++ loops.js
@@ -0,0 +1,22 @@
+// for(let i = 1; i <= 100; i++){
+//     console.log(i)
+// }
+
+// for(let i = 0; i <= 25; i+=5){
+//     console.log(i)
+// }
+
+let name = 'Ginny Munson'
+for(let i = 0; i <= name.length; i++){
+    console.log(name[i])
+}
+
+for(c of name){
+    console.log(c)
+}
+
+let i = 0
+while(i < 10){
+    console.log(i)
+    i++
+}

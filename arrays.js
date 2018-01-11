588afee80ed3f42d87f7ab61825fc870f35db0db
vmunson <virginia.munson@gmail.com>
2018-01-11 13:13:41 -0500

    array examples

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let fruits = ['banana', 'apple', 'pineapple', 'mango', 'lemon']
+let junk = ['scredriver' 15, ['post-it', 'keys'], false]
+console.log(fruits)
+
+for(fruit of fruits){
+    console.log(fruit)
+}

1b260b2c568ec50add9d2357f160a5500e59335e
vmunson <virginia.munson@gmail.com>
2018-01-16 09:23:09 -0500

    this

--- objects.js
+++ objects.js
@@ -7,9 +7,9 @@ let myCar = {
     showInfo: function(){
         console.log(myCar.make, myCar.model)
     },
-    drive: function(miles){
-        myCar.miles += miles
-        console.log(myCar.miles)
+    // drive: function(m){
+    //     this.miles += m
+    //     console.log(`Odometer: ${this.miles}`)
     }
 }
 
@@ -17,3 +17,8 @@ myCar.year = 2020
 console.log(`My car is a ${myCar.year} ${myCar.make} ${myCar.model}, it has ${myCar.miles} miles.`)
 
 myCar.drive(100)
+
+function drive(car, m){
+    car.miles += m
+    console.log(`Odometer: ${car.miles}`)
+}
